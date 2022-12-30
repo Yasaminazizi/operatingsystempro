cc696c74bd98662562ec62e876ea57891f82ea44
Yasaminazizi <yasamin.azizi8@gmail.com>
2022-12-30 21:06:36 +0330

    Update os.c
    finaly

--- os.c
+++ os.c
@@ -8,35 +8,33 @@
 #include<readline/readline.h>
 #include<readline/history.h>
 
-#define MAXCOM 1000 // max number of letters to be supported
-#define MAXLIST 100 // max number of commands to be supported
 
-// Clearing the shell using escape sequences
-#define clear() printf("\033[H\033[J")
+
 
 // Greeting shell during startup
 void init_shell()
 {
-	clear();
+	printf("\033[H\033[J");
 	printf("\n\n\n\n");
-	printf("\n\n\n\t****Linux Shell****");
+	printf("\n\n\n\t****Welcome****");
 	printf("\n\n\n\n");
-	char* username = getenv("USER");
-	printf("\n\n\nWelcome @%s", username);
-	printf("\n");
-	sleep(1);
-	clear();
+	printf("\n\n\n if you don't know what commands to use,");
+	printf("\nplease enter the 'help' command first.");
+	printf("\n\n\n");
 }
 
-// Function to take input
+// Function to take input & history
 int takeInput(char* str)
 {
 	char* buf;
-
 	buf = readline("\n>>> ");
+	FILE *history;
+	history = fopen("history.txt" , "a");
 	if (strlen(buf) != 0) {
 		add_history(buf);
 		strcpy(str, buf);
+		fprintf(history,"%s\n", buf);
+		fclose(history);
 		return 0;
 	} else {
 		return 1;
@@ -62,7 +60,7 @@ void execArgs(char** parsed)
 		return;
 	} else if (pid == 0) {
 		if (execvp(parsed[0], parsed) < 0) {
-			printf("\nCould not execute command.. avali");
+			printf("\nCould not execute command..");
 		}
 		exit(0);
 	} else {
@@ -72,238 +70,253 @@ void execArgs(char** parsed)
 	}
 }
 
-// Function where the piped system commands is executed
-void execArgsPiped(char** parsed, char** parsedpipe)
-{
-	// 0 is read end, 1 is write end
-	int pipefd[2];
-	pid_t p1, p2;
-
-	if (pipe(pipefd) < 0) {
-		printf("\nPipe could not be initialized");
-		return;
-	}
-	p1 = fork();
-	if (p1 < 0) {
-		printf("\nCould not fork");
-		return;
-	}
-
-	if (p1 == 0) {
-		// Child 1 executing..
-		// It only needs to write at the write end
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[1]);
-
-		if (execvp(parsed[0], parsed) < 0) {
-			printf("\nCould not execute command 1.. dovomi");
-			exit(0);
-		}
-	} else {
-		// Parent executing
-		p2 = fork();
-
-		if (p2 < 0) {
-			printf("\nCould not fork");
-			return;
-		}
-
-		// Child 2 executing..
-		// It only needs to read at the read end
-		if (p2 == 0) {
-			close(pipefd[1]);
-			dup2(pipefd[0], STDIN_FILENO);
-			close(pipefd[0]);
-			if (execvp(parsedpipe[0], parsedpipe) < 0) {
-				printf("\nCould not execute command 2..");
-				exit(0);
-			}
-		} else {
-			// parent executing, waiting for two children
-			wait(NULL);
-			wait(NULL);
-		}
-	}
-}
-
-// Help command builtin
+// Help command 
 void openHelp()
 {
-	puts("\n***WELCOME TO MY SHELL HELP***"
-		"\nCopyright @ Suprotik Dey"
-		"\n-Use the shell at your own risk..."
+	puts("\n***WELCOME TO MY SHELL ***"
+		
 		"\nList of Commands supported:"
 		"\n>cd"
 		"\n>ls"
 		"\n>pwd"
 		"\n>exit"
-		"\n>Display the number of file lines"
-		"\n>Display the first ten of the file"
-		"\n>all other general commands available in UNIX shell"
-		"\n>pipe handling"
-		"\n>improper space handling");
-
+		"\n>crtl+c:The program does not end with crtl+c"
+		"\n>firstWord:Returns the first part of each line separated by spaces in the file"
+		"\n>repeated:Display the most repeated word in the file"
+		"\n>noComment:show lines that are not comment of the file"
+		"\n>noSpace:Remove all empty spaces from the file and display it"
+		"\n>line:Display the number of file lines"
+		"\n>tenLine:Display the first ten lines of the file"
+		"\n>history:entered commands are stored in a file called history  "
+		"\n and by pressing the up & down button finds the previous commands."
+		"\n>all other general commands available in UNIX shell");
+	
 	return;
 }
 
-
+// Display the number of file lines
 void line(char address[])
 {
-	pid_t pid = fork();
-
-	if (pid == -1) {
-		printf("\nFailed forking child..");
-		return;
-	} else if (pid == 0) {
-		FILE *fp;
-		char c;
-		int count = 0;
-		fp = fopen(address, "r");
-		if (fp==NULL)
-		{
-			return;
-		}
-		for(c = getc(fp); c != EOF; c = getc(fp))
-			if(c == '\n')
-				count = count + 1;
-		fclose(fp);
-		printf("%d\n",count);
-		return;
-			exit(0);
-	} else {
-		// waiting for child to terminate
-		wait(NULL);
-		return;
-	}
 	
+	FILE *fp;
+	char c;
+	int count = 0;
+	fp = fopen(address, "r");
+	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}
+	for(c = getc(fp); c != EOF; c = getc(fp))
+		if(c == '\n')
+			count = count + 1;
+	fclose(fp);
+	printf("Lines: %d\n",count);
+	return;	
 }
+
+// crtl+c
 void sigintHandler(int sig_num)
-{
-	
-    /* Reset handler to catch SIGINT next time.*/
-	
-    signal(SIGINT, sigintHandler);
-    printf("\n Cannot be terminated using Ctrl+C \n");
-    fflush(stdout);
-	printf(">>> ");
-	
+{	
+ 	signal(SIGINT, sigintHandler);
+        printf("\n Cannot be terminated using Ctrl+C \n");
+        printDir();
+        fflush(stdout);
+        printf("\n>>> ");
 }
 
+//display the first ten line of file
 void tenLine(char address[])
 {
-	pid_t pid = fork();
+	
+	FILE *fp;
+	char c;
+	char s[1000] = {'\0'};
+	int count = 0;
+	int n = 0;
+	fp = fopen(address, "r");
+	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}
+	for(c = getc(fp); c != EOF && count < 10; c = getc(fp))
+	{
+		s[n] = c;
+		n = n + 1;
+		if(c == '\n')
+			count = count + 1;
+	}
+	fclose(fp);
+	printf("First 10 lines of file:\n");
+	puts(s);
+	return;	
+}
 
-	if (pid == -1) {
-		printf("\nFailed forking child..");
-		return;
-	} else if (pid == 0) {
-		FILE *fp;
-		char c;
-		char s[1000] = {'\0'};
-		int count = 0;
-		int n = 0;
-		fp = fopen(address, "r");
-		if (fp==NULL)
-		{
-			return;
-		}
-		for(c = getc(fp); c != EOF && count < 10; c = getc(fp))
+//display lines of file with no space
+void noSpace(char address[])
+{
+	
+	FILE *fp;
+	char c;
+	char s[1000] = {'\0'};
+	int count = 0;
+	int n = 0;
+	fp = fopen(address, "r");
+	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}
+	for(c = getc(fp); c != EOF ; c = getc(fp))
+	{
+		if(c != '\n' && c != '\t' && c != ' ')
 		{
 			s[n] = c;
 			n = n + 1;
-			if(c == '\n')
-				count = count + 1;
 		}
-		fclose(fp);
-		puts(s);
-		return;
-			exit(0);
-	} else {
-		// waiting for child to terminate
-		wait(NULL);
-		return;
 	}
-	
+	fclose(fp);
+	printf("File with no spaces:\n");
+	puts(s);
+	return;	
 }
-//repeated word
-void repeated(char address[]){
-pid_t pid = fork();
 
-	if (pid == -1) {
-		printf("\nFailed forking child..");
-		return;
-	} else if (pid == 0) {
-	 FILE *fp;
-	 char ch, *line;  
-     size_t len = 0, read;  
-     char words[1000][1000], word[20];  
-     int i = 0, j, k, maxCount = 0, count;  
-	 fp = fopen(address,"r"); 
-	  if (fp== NULL){  
-        printf("File not found");  
-        exit(EXIT_FAILURE);  
-    }  
+//display the first word of line in file
+void firstWord(char address[])
+{
+	
+	FILE *fp;
+	char c;
+	char s[1000] = {'\0'};
+	int count = 0;
+	int n = 0;
+	fp = fopen(address, "r");
+	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}
+	for(c = getc(fp); c != EOF ; c = getc(fp))
+	{
+		
+		if(c == '\t' || c == ' ')
+		{
+			count = 1;
+		}
+		if(c == '\n')
+		{
+			count = 0;
+		}
+		if(count == 0)
+		{
+			s[n] = c;
+			n = n + 1;
+		}
+	}
+	fclose(fp);
+	printf("First words of file:\n");
+	puts(s);
+	return;	
+}
+
+//display the most repeated word of file
+void repeated(char address[])
+{
+	FILE *fp;
+ 	char ch, *line;  
+   	size_t len = 0, read;  
+     	char words[1000][1000], word[20];  
+     	int i = 0, j, k, maxCount = 0, count;  
+	fp = fopen(address,"r"); 
+  	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}	  
+ 
+    	while ((read = getline(&line, &len, fp)) != -1) {      
+	       for(k=0; line[k]!='\0'; k++){     
+            		if(line[k] != ' ' && line[k] != '\n' && line[k] != ',' && line[k] != '.' ){  
+            			   words[i][j++] = tolower(line[k]);  
+            		}  
+            		else{  
+                		   words[i][j] = '\0';   
+                		   i++;  
+                		   j = 0;
+                        }  
+       		 }		  
+    	}  
       
-    //Since, C doesn't provide in-built function,   
-    //following code will split content of file into words  
-    while ((read = getline(&line, &len, fp)) != -1) {  
-          
-        for(k=0; line[k]!='\0'; k++){  
-            //Here, i represents row and j represents column of two-dimensional array words   
-            if(line[k] != ' ' && line[k] != '\n' && line[k] != ',' && line[k] != '.' ){  
-                words[i][j++] = tolower(line[k]);  
-            }  
-            else{  
-                words[i][j] = '\0';  
-                //Increment row count to store new word  
-                i++;  
-                //Set column count to 0  
-                j = 0;  
-            }  
-        }  
-    }  
-      
-    int length = i;  
-      
-    //Determine the most repeated word in a file  
-    for(i = 0; i < length; i++){  
-        count = 1;  
-        //Count each word in the file and store it in variable count  
-        for(j = i+1; j < length; j++){  
-            if(strcmp(words[i], words[j]) == 0 && (strcmp(words[i]," ") != 0)){  
-                count++;  
-            }   
-        }  
-        //If maxCount is less than count then store value of count in maxCount   
-        //and corresponding word to variable word  
-        if(count > maxCount){  
-            maxCount = count;  
-            strcpy(word, words[i]);  
-        }  
-    }  
-      
-    printf("Most repeated word: %s", word);  
-    fclose(fp);  
-      
-    //return 0;  
- }  
-}	
+    	int length = i;    
+    		for(i = 0; i < length; i++){  
+        		count = 1;   
+        		for(j = i+1; j < length; j++){  
+            			if(strcmp(words[i], words[j]) == 0 && (strcmp(words[i]," ") != 0)){  
+                			count++;  
+            			}   
+        		}   
+        		if(count > maxCount){  
+            			maxCount = count;  
+            			strcpy(word, words[i]);  
+        		}  
+    		}  
+        printf("Most repeated word: %s", word);  
+        fclose(fp);  
+        return ;
+}
+
+//display file with no comment
+void noComment(char address[])
+{
+	FILE *fp;
+	char c;
+	int m=0;
+	char s[10000] = {'\0'};
+	int count = 0;
+	int n = 0;
+	fp = fopen(address, "r");
+	if (fp== NULL){  
+       		printf("File not found");  
+       		return;  
+    	}
+	for(c = getc(fp) ; c != EOF ; c = getc(fp))
+	{
+		if(count == 0)
+		{
+			if(c == '#')
+			{
+				m=1;
+			}
+			count = 1;
+		}
+		if(m==0)
+		{
+			s[n] = c;
+			n = n + 1;
+		}
+		if(c == '\n'){
+			count = 0;
+			m=0;
+		}	
+	}
+	fclose(fp);
+	printf("File with no comments:\n");
+	puts(s);
+	return;	
+}
 
 // Function to execute builtin commands
 int ownCmdHandler(char** parsed)
 {
-	int NoOfOwnCmds = 7, i, switchOwnArg = 0;
+	int NoOfOwnCmds = 9, i, switchOwnArg = 0;
 	char* ListOfOwnCmds[NoOfOwnCmds];
 	char* username;
 
 	ListOfOwnCmds[0] = "exit";
 	ListOfOwnCmds[1] = "cd";
 	ListOfOwnCmds[2] = "help";
-	ListOfOwnCmds[3] = "hello";
+	ListOfOwnCmds[3] = "firstWord";
 	ListOfOwnCmds[4] = "line";
 	ListOfOwnCmds[5] = "tenLine";
 	ListOfOwnCmds[6] = "repeated";
+	ListOfOwnCmds[7] = "noComment";
+	ListOfOwnCmds[8] = "noSpace";
 
 	for (i = 0; i < NoOfOwnCmds; i++) {
 		if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
@@ -311,56 +324,64 @@ int ownCmdHandler(char** parsed)
 			break;
 		}
 	}
+	pid_t pid = fork();
 
+	if (pid == -1) {
+		printf("\nFailed forking child..");		
+		return 1;
+	} else if (pid == 0) {
+	
 	switch (switchOwnArg) {
 	case 1:
 		printf("\nGoodbye\n");
-		exit(0);
+		kill(0,SIGKILL);
 		exit(0);
 	case 2:
 		chdir(parsed[1]);
 		return 1;
+		exit(0);
 	case 3:
 		openHelp();
+		exit(0);
 		return 1;
 	case 4:
-		username = getenv("USER");
-		printf("\nHello %s.\nMind that this is "
-			"not a place to play around."
-			"\nUse help to know more..\n",
-			username);
+		firstWord(parsed[1]);
+		exit(0);
 		return 1;
 	case 5:
 		line(parsed[1]);
+		exit(0);
 		return 1;
 	case 6:
 		tenLine(parsed[1]);
+		exit(0);
 		return 1;
 	case 7:
 		repeated(parsed[1]);
+		exit(0);
 		return 1;
+	case 8: 
+		noComment(parsed[1]);
+		exit(0);
+		return 1;
+	case 9: 
+		noSpace(parsed[1]);
+		exit(0);
+		return 1;
+		
 	default:
 		break;
 	}
-
-	return 0;
-}
-
-// function for finding pipe
-int parsePipe(char* str, char** strpiped)
-{
-	int i;
-	for (i = 0; i < 2; i++) {
-		strpiped[i] = strsep(&str, "|");
-		if (strpiped[i] == NULL)
-			break;
+	
 	}
-
-	if (strpiped[1] == NULL)
-		return 0; // returns zero if no pipe is found.
-	else {
+	else
+	{
+	// waiting for child to terminate
+		wait(NULL);
 		return 1;
 	}
+
+	return 0;
 }
 
 // function for parsing command words
@@ -368,7 +389,7 @@ void parseSpace(char* str, char** parsed)
 {
 	int i;
 
-	for (i = 0; i < MAXLIST; i++) {
+	for (i = 0; i < 100; i++) {
 		parsed[i] = strsep(&str, " ");
 
 		if (parsed[i] == NULL)
@@ -378,57 +399,38 @@ void parseSpace(char* str, char** parsed)
 	}
 }
 
-int processString(char* str, char** parsed, char** parsedpipe)
+int processString(char* str, char** parsed)
 {
-
-	char* strpiped[2];
-	int piped = 0;
-
-	piped = parsePipe(str, strpiped);
-
-	if (piped) {
-		parseSpace(strpiped[0], parsed);
-		parseSpace(strpiped[1], parsedpipe);
-
-	} else {
-
-		parseSpace(str, parsed);
-	}
-
+	parseSpace(str, parsed);
 	if (ownCmdHandler(parsed))
 		return 0;
 	else
-		return 1 + piped;
+		return 1;
 }
 
 int main()
 {
-	char inputString[MAXCOM], *parsedArgs[MAXLIST];
-	char* parsedArgsPiped[MAXLIST];
+	FILE *history;
+	history = fopen("history.txt" , "w");
+	
+	
+	char inputString[100], *parsedArgs[100];
 	int execFlag = 0;
 	init_shell();
 	signal(SIGINT, sigintHandler);
 
 	while (1) {
-		// print shell line
+		
 		printDir();
-		// take input
+		
 		if (takeInput(inputString))
 			continue;
-		// process
+		
 		execFlag = processString(inputString,
-		parsedArgs, parsedArgsPiped);
-		// execflag returns zero if there is no command
-		// or it is a builtin command,
-		// 1 if it is a simple command
-		// 2 if it is including a pipe.
-
-		// execute
+		parsedArgs);
+		
 		if (execFlag == 1)
 			execArgs(parsedArgs);
-
-		if (execFlag == 2)
-			execArgsPiped(parsedArgs, parsedArgsPiped);
 	}
 	return 0;
 }
